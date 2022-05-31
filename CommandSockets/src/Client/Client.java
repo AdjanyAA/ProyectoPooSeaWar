66fa9521752b2b9a940e11cc77af7f899a8a2b2f
AdjanyAA <106577737+AdjanyAA@users.noreply.github.com>
2022-05-31 00:32:23 -0600

    Add files via upload

---
+++ CommandSockets/src/Client/Client.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author diemo
+ */
+public class Client {
+    private final int PORT = 35500;
+    private final String SERVER_IP = "localhost";
+    public Socket socket;
+    public ObjectOutputStream writer;
+    public ClientFrame screenRef;
+    private boolean isConnected = false;
+    private ThreadClient theadClient; 
+    protected String nombre;
+    protected ArrayList<Luchador> luchadores;
+    
+    public Client(ClientFrame screenRef) {
+        this.screenRef = screenRef;
+    }
+    
+    public void connect(){
+        try {
+            if (!isConnected){
+                nombre = JOptionPane.showInputDialog("Introducir el nombre del jugador:");
+                this.socket = new Socket(SERVER_IP, PORT);
+                this.writer = new ObjectOutputStream(socket.getOutputStream());
+                this.screenRef.showClientMessage("Conectado al server");
+                this.theadClient = new ThreadClient(this);
+                this.theadClient.start();
+                this.isConnected = true;
+            }
+            //threadClient
+        } catch (IOException ex) {           
+        }
+    }
+    
+    
+}
---
+++ CommandSockets/src/Client/Luchador.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Client;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Luchador {
+    
+    
+    private String nombre;
+    private String imagen;
+    private int porcentaje;
+    
+    private int poder;
+    private int resistencia;
+    private int autosanidad;
+
+    public Luchador(String nombre, String imagen, int porcentaje, int poder, int resistencia, int autosanidad) {
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.porcentaje = porcentaje;
+        this.poder = poder;
+        this.resistencia = resistencia;
+        this.autosanidad = autosanidad;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
